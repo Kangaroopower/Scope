5c95f3b4ac0d997e6e56fcb0677a5ac93856baec
Kangaroopower <racecar360@gmail.com>
2012-06-02 17:18:40 -0700

    add regex support for replaceall

--- Actions.js
+++ Actions.js
@@ -1,30 +1,38 @@
 // Find and Replace Actions
- 		window.FindReplace.Actions.replaceall = function () {
+		window.FindReplace.Actions.replaceall = function (regex) {
 			var txtofind,
 				txtarea = $('textarea')[0].value,
 				rawtxtofind = $('#fr-find-text').val(),
 				txtoreplace = $('#fr-replace-text').val();
-			if ($('input#fr-cs').is(':checked')) {
-				txtofind = RegExp(rawtxtofind, 'g');
+			if (regex === true) {
+				if ($('input#fr-cs').is(':checked')) {
+					txtofind = RegExp(rawtxtofind, 'g');
+				} else {
+					txtofind = RegExp(rawtxtofind,'ig');
+				}
 			} else {
-				txtofind = RegExp(rawtxtofind,'ig');
+				txtofind = rawtxtofind;
 			}
 			var thematches = txtarea.match(txtofind).length;
 			$('textarea')[0].value = $('textarea')[0].value.replace(txtofind, txtoreplace);
 			$("#fr-status").html( thematches+' replacement(s) made!');
 		};
  
-		window.FindReplace.Actions.replace = function () {
+		window.FindReplace.Actions.replace = function (regex) {
 			var txtofind,
 				rawtxtofind = $('#fr-find-text').val(),
 				txtoreplace = $('#fr-replace-text').val();
-			if ($('input#fr-cs').is(':checked')) {
-				txtofind = RegExp(rawtxtofind);
+			if (regex === true) {
+				if ($('input#fr-cs').is(':checked')) {
+					txtofind = RegExp(rawtxtofind);
+				} else {
+					txtofind = RegExp(rawtxtofind,'i');
+				}
+				$('textarea')[0].value = $('textarea')[0].value.replace(txtofind, txtoreplace);
+				$("#fr-status").html('One Replacement made.');
 			} else {
-				txtofind = RegExp(rawtxtofind,'i');
+				$("#fr-status").html('Sorry, non-Regex FindReplace is currently not supported for single replaces, only for replace all.');
 			}
-			$('textarea')[0].value = $('textarea')[0].value.replace(txtofind, txtoreplace);
-			$("#fr-status").html('One Replacement made.');
 		};
  
 		window.FindReplace.Actions.find = function () {
@@ -39,5 +47,20 @@
 			window.FindReplace.Shadow.regex = regex;
 			window.FindReplace.Shadow.synch();
 		};
-    
+
+		window.FindReplace.Actions.regcheck = function (type) {
+			if (type == "null") {
+				if ($('input#fr-reg').is(':checked')) {
+					window.FindReplace.Actions.replace(true);
+				} else {
+					window.FindReplace.Actions.replace(false);
+				}
+			} else {
+				if ($('input#fr-reg').is(':checked')) {
+					window.FindReplace.Actions.replaceall(true);
+				} else {
+					window.FindReplace.Actions.replaceall(false);
+				}
+			}
+		};
     window.FindReplace.registerModule("Actions")
