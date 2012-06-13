eb0e518b4fb34d357956aca3ca7cc05c106c3fa1
Kangaroopower <racecar360@gmail.com>
2012-06-13 10:13:19 -0700

    remove half done regex support... Some bugfixes

--- Actions.js
+++ Actions.js
@@ -1,66 +1,45 @@
-// Find and Replace Actions
-		window.FindReplace.Actions.replaceall = function (regex) {
-			var txtofind,
-				txtarea = $('textarea')[0].value,
-				rawtxtofind = $('#fr-find-text').val(),
-				txtoreplace = $('#fr-replace-text').val();
-			if (regex === true) {
-				if ($('input#fr-cs').is(':checked')) {
-					txtofind = RegExp(rawtxtofind, 'g');
-				} else {
-					txtofind = RegExp(rawtxtofind,'ig');
-				}
-			} else {
-				txtofind = rawtxtofind;
-			}
-			var thematches = txtarea.match(txtofind).length;
-			$('textarea')[0].value = $('textarea')[0].value.replace(txtofind, txtoreplace);
-			$("#fr-status").html( thematches+' replacement(s) made!');
-		};
+// Find and Replace Module
+window.FindReplace.Actions.replaceall = function () {
+	var txtofind,
+		txtarea = $('textarea')[0].value,
+		rawtxtofind = $('#fr-find-text').val(),
+		txtoreplace = $('#fr-replace-text').val();
+	if ($('input#fr-cs').is(':checked')) {
+		txtofind = RegExp(rawtxtofind, 'g');
+	} else {
+		txtofind = RegExp(rawtxtofind,'ig');
+	}
+	var thematches = txtarea.match(txtofind).length;
+	$('textarea')[0].value = $('textarea')[0].value.replace(txtofind, txtoreplace);
+	if (thematches != "undefined") {
+		$("#fr-status").html( thematches+' replacement(s) made!');
+	} else {
+		$("#fr-status").html( 'No replacement(s) made!');
+	}
+};
  
-		window.FindReplace.Actions.replace = function (regex) {
-			var txtofind,
-				rawtxtofind = $('#fr-find-text').val(),
-				txtoreplace = $('#fr-replace-text').val();
-			if (regex === true) {
-				if ($('input#fr-cs').is(':checked')) {
-					txtofind = RegExp(rawtxtofind);
-				} else {
-					txtofind = RegExp(rawtxtofind,'i');
-				}
-				$('textarea')[0].value = $('textarea')[0].value.replace(txtofind, txtoreplace);
-				$("#fr-status").html('One Replacement made.');
-			} else {
-				$("#fr-status").html('Sorry, non-Regex FindReplace is currently not supported for single replaces, only for replace all.');
-			}
-		};
+window.FindReplace.Actions.replace = function () {
+	var txtofind,
+		rawtxtofind = $('#fr-find-text').val(),
+		txtoreplace = $('#fr-replace-text').val();
+	if ($('input#fr-cs').is(':checked')) {
+		txtofind = RegExp(rawtxtofind);
+	} else {
+		txtofind = RegExp(rawtxtofind,'i');
+	}
+	$('textarea')[0].value = $('textarea')[0].value.replace(txtofind, txtoreplace);
+	$("#fr-status").html('One Replacement made.');
+};
  
-		window.FindReplace.Actions.find = function () {
-			var regex;
-			if ($('input#fr-cs').is(':checked')) {
-				regex = RegExp(rawtxtofind);
-			} else {
-				regex = RegExp(rawtxtofind,'i');
-			}
-			$('#fr-find-prev').attr('disabled', null === regex);
-			$('#fr-find-next').attr('disabled', null === regex);
-			window.FindReplace.Shadow.regex = regex;
-			window.FindReplace.Shadow.synch();
-		};
-
-		window.FindReplace.Actions.regcheck = function (type) {
-			if (type == "null") {
-				if ($('input#fr-reg').is(':checked')) {
-					window.FindReplace.Actions.replace(true);
-				} else {
-					window.FindReplace.Actions.replace(false);
-				}
-			} else {
-				if ($('input#fr-reg').is(':checked')) {
-					window.FindReplace.Actions.replaceall(true);
-				} else {
-					window.FindReplace.Actions.replaceall(false);
-				}
-			}
-		};
-    window.FindReplace.registerModule("Actions")
+window.FindReplace.Actions.find = function () {
+	var regex;
+	if ($('input#fr-cs').is(':checked')) {
+		regex = RegExp(rawtxtofind);
+	} else {
+		regex = RegExp(rawtxtofind,'i');
+	}
+	$('#fr-find-prev').attr('disabled', null === regex);
+	$('#fr-find-next').attr('disabled', null === regex);
+	window.FindReplace.Shadow.regex = regex;
+	window.FindReplace.Shadow.synch();
+};
