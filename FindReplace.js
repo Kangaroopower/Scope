b1e241bfed0ffe7b32cdbd3a61da5eed1a8eac75
Kangaroopower <racecar360@gmail.com>
2012-06-16 13:37:28 -0700

    Use fonts, not pictures, import modules

--- FindReplace.js
+++ FindReplace.js
@@ -5,13 +5,14 @@ $('document').ready(function( ) {
 	}
 	//Base for functions
 	window.FindReplace = {
-		version: "2.3.7.5 Dev"
+		version: "2.3.8.5 Dev"
 		modules: [],
 		active: false
 	};
  
 		/* Initialize the script */
 		window.FindReplace.init = function () {
+			importStylesheetURI('http://kangaroopower.x10.mx/css/font-awesome.css');
 			importScriptURI('//ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js');
 			importScriptPage('textinputs_jquery.js', 'dev');
 			if (wgAction === "edit") {
@@ -22,22 +23,24 @@ $('document').ready(function( ) {
 						$('#toolbar').append('<a href="#" onclick="window.FindReplace.GUI.initiate();"><img title="Replace" src="http://images2.wikia.nocookie.net/__cb20120415071129/central/images/7/71/Replace.png"></a>');
 					}
 				}
+				importScriptURI('https://raw.github.com/Kangaroopower/FindReplace/master/Actions.js');
+				importScriptURI('https://raw.github.com/Kangaroopower/FindReplace/master/Gui.js');
+				importScriptURI('https://raw.github.com/Kangaroopower/FindReplace/master/Shadow.js');
 			}
 		};
 
 		window.FindReplace.waitForEditor = function () {
-			if (typeof (WikiaEditor || WikiaEditor.getInstance || WikiaEditor.getInstance || WikiaEditor.getInstance() || WikiaEditor.getInstance().mode) == 'undefined'  ||
-            WikiaEditor.getInstance().mode !== 'source') {
-            	window.setTimeout(function () {
-                	console.log('waiting...');
-                	window.FindReplace.waitForEditor();
-            	}, 500);
-           	 return;
-        	} else {
-        		window.FindReplace.editorloaded = true;
-        		window.FindReplace.init();
-        	}
-		}
+			if (typeof (WikiaEditor || WikiaEditor.getInstance || WikiaEditor.getInstance || WikiaEditor.getInstance() || WikiaEditor.getInstance().mode) == 'undefined'  || WikiaEditor.getInstance().mode !== 'source') {
+				window.setTimeout(function () {
+					console.log('waiting...');
+					window.FindReplace.waitForEditor();
+				}, 500);
+				return;
+			} else {
+				window.FindReplace.editorloaded = true;
+				window.FindReplace.init();
+			}
+		};
 
 		window.FindReplace.registerModule = function (module) {
 			window.FindReplace.modules.push(module); 
@@ -61,5 +64,5 @@ $('document').ready(function( ) {
 		}
 	});
 
-	$(document).ready(window.FindReplace.waitForEditor);
+	if (wgAction === 'edit') $(document).ready(window.FindReplace.waitForEditor);
 });
