5d7707f227c40ade043e90428f961bc854d26e06
Kangaroopower <racecar360@gmail.com>
2012-08-14 17:09:13 -0700

    Complete overhaul using Sequencer
    The Load process is completely controlled by sequencer and the
    waitforeditor process has been drastically changed. Keyboard shortcuts
    have been made smaller and the program has been upgraded to 3.0 Dev

--- Load.js
+++ Load.js
@@ -13,86 +13,74 @@ $('document').ready(function( ) {
 	
 	//Base for functions
 	window.Scope = {
-		version: "2.74 Dev",
-		editorloaded: false,
-		librariesready: false,
+		version: "3.0 Dev",
 		libraries: {
-			'jQueryUI': 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js',
-			'TextInputs': 'http://dev.wikia.com/index.php?title=textinputs_jquery.js&action=raw&ctype=text/javascript'
+			'jQueryUI': '//ajax.googleapis.com/ajax/libs/jqueryui/1.8.22/jquery-ui.min.js',
+			'TextInputs': 'http://dev.wikia.com/index.php?title=textinputs_jquery.js&action=raw&ctype=text/javascript',
+			'Dialog': 'http://kangaroopower.wikia.com/index.php?title=MediaWiki:Scope.js/gui.js&action=raw&ctype=text/javascript'
 		},
-		modulesReady: false,
 		modules: {
 			'Actions', 'https://raw.github.com/Kangaroopower/FindReplace/master/Actions.js',
 			'GUI', 'https://raw.github.com/Kangaroopower/FindReplace/master/Gui.js',
 			'Shadow', 'https://raw.github.com/Kangaroopower/FindReplace/master/Shadow.js'
 		},
-		active: false
+		Shadow: {
+			'matches': [],
+			'highlighted': -1,
+			'nTrav': 0
+		}
 	};
-	var Scope = window.Scope, sctextarea;
+	//Vars
+	var Scope = window.Scope, sctxtarea, sequence = {}, scfind = $('#sc-find').val();
 
-		Scope.waitForEditor = function () {
-			if (wgAction === "edit" && wgCanonicalNamespace !== ("Message_Wall" || "Thread") ) {
-				if (typeof (WikiaEditor || WikiaEditor.getInstance || WikiaEditor.getInstance ||WikiaEditor.getInstance() || WikiaEditor.getInstance().mode) == 'undefined'  ||WikiaEditor.getInstance().mode !== 'source') {
-					window.setTimeout(function () {
-						console.log('waiting for editor...');
-							Scope.waitForEditor();
-						}, 500);
-					return;
-				} else {
-					sctextarea = WikiaEditor.getInstance().getEditbox();
-					Scope.editorloaded = true;
-					Scope.loadLibraries();
-					console.log('Scope: Editor Loaded');
-				}
-			}
+		/* Load editor before everything else (and make sure it's in source mode!)*/
+		Scope.load = function () {
+			if (window.RTE && RTE.getInstance && RTE.getInstance()) {
+				if ('source' === RTE.getInstance().mode) Scope.Sequencer('editor').loaded();
+				} else if ('wysiwyg' === RTE.getInstance().mode) {
+					$('#sc-shadow, #sc-ui').remove();
+					sctxtarea.removeAttr('style');
+				} else console.log('Cannot detect editor');
+			} else if (window.CKEDITOR) {
+				CKEDITOR.on('instanceReady', function () {
+					RTE.getInstance().on('wysiwygModeReady',Scope.GUI.close());
+					RTE.getInstance().on('sourceModeReady',Scope.Sequencer('editor').loaded());
+				});
+			} else if (window.WikiaEditor) {
+				if (WikiaEditor.getInstance && WikiaEditor.getInstance()) {
+					if ('source' === WikiaEditor.getInstance().mode) Scope.Sequencer('editor').loaded();
+					} else if ('wysiwyg' === WikiaEditor.getInstance().mode) {
+						$('#sc-shadow, #sc-ui').remove();
+						sctxtarea.removeAttr('style');
+					} else console.log('Cannot detect editor');
+				} else if (GlobalTriggers) GlobalTriggers.on('WikiaEditorReady', Scope.Sequencer('editor').loaded());
+				else console.log('Cannot detect editor');
+			} else console.log('Cannot detect editor');
 		};
  
-		Scope.loadLibraries =  function () {
-			if (Scope.librariesready !== true) {
-				for (var i in Scope.libraries) {
-					$.getScript(Scope.libraries[i], function () {
-						console.log('Scope: '+ i +' is ready');
-					});
-				}
-				Scope.librariesready = true;
-				window.setTimeout(function () {
-					console.log('Scope: waiting for libraries...');
-						Scope.loadLibraries();
-				}, 500);				
-			} else {
-				Scope.loadModules();
-				console.log('Scope: Libraries Loaded');
-			}
-		};
+		/* Load libraries before modules */
+		Scope.Sequencer('editor').onload(function () {
+			sctxtarea = WikiaEditor.getInstance().getEditbox();
+			console.log('Scope: Editor Loaded');
+			for (var i in Scope.libraries) $.getScript(Scope.libraries[i], console.log('Scope: '+ i +' is ready'));
+			Scope.Sequencer('lib').loaded();
+			console.log('Loaded: Scope', Scope.version);
+		}());
 
-		Scope.loadModules = function () {
-			var Shadowready, GUIready, Actionsready;
-			if (Scope.modulesReady !== true) {
-				for (var i in Scope.modules) {
-					$.getScript(Scope.modules[i], function () {
-						console.log('Scope Module: '+ i +' is ready');
-					});
-				}
-				Scope.modulesReady = true;
-				window.setTimeout(function () {
-					console.log('waiting for modules...');
-					Scope.loadModules();
-				}, 500);
-			} else {
-				$('span.cke_toolbar_expand').before('<a style="cursor:pointer;" onclick="Scope.GUI.initiate();"><img title="Replace" src="http://images2.wikia.nocookie.net/__cb20120415071129/central/images/7/71/Replace.png"></a>');	
-				console.log('Scope: Modules Loaded');
-				console.log('Loaded: Scope', Scope.version);
-			}
-		};
+		/* Load modules before script */
+		Scope.Sequencer('lib').onload(function () {
+			for (var i in Scope.modules) $.getScript(Scope.modules[i], console.log('Scope Module: '+ i +' is ready'));
+			$('span.cke_toolbar_expand').before('<a style="cursor:pointer;" onclick="Scope.GUI.initiate();"><img title="Replace" src="http://images2.wikia.nocookie.net/__cb20120415071129/central/images/7/71/Replace.png"></a>');	
+			console.log('Scope: Modules Loaded');
+			Scope.Sequencer('modules').loaded();
+			console.log('Loaded: Scope', Scope.version);
+		}());
 
-	var isCtrl = false;
-	$(document).keyup(function (e) {
-		if(e.which === 17) isCtrl = false;
-	}).keydown(function (e) {
-		if(e.which === 17) isCtrl = true;
-		if(e.which === 32 && isCtrl === true) {
+	/* Keyboard Shortcuts- Ctrl + Space to open and close the dialog */
+	$(document).keydown(function (e) {
+		if (e.which === 32 && e.ctrlKey) {
 			e.preventDefault();
-			if (Scope.active === true) Scope.GUI.close();
+			if (document.querySelector('#sc-ui')) Scope.GUI.close();
 			else Scope.GUI.initiate();
 		}
 	});
