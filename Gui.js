dfbd13932e013b5224847971a9d6f0666d7304f1
Kangaroopower <racecar360@gmail.com>
2012-08-15 12:05:17 -0700

    Merge Shadow's GUI into dialog GUI function

--- Gui.js
+++ Gui.js
@@ -1,6 +1,6 @@
 /* GUI */
 	/* GUI operations- open/close (Actual dialog is stored elsewhere) */
-	Scope.Sequencer('lib').onload(function () {
+	Scope.Sequencer('modules').onload(function () {
 		$('#sc-start').click(function () {
 			if (document.querySelector('#sc-ui')) {
 				$('#sc-shadow, #sc-ui').remove();
@@ -9,22 +9,17 @@
 				$('#editform').prepend(Scope.dialog);
 				$('#sc-find, #sc-cs, '+ sctxtarea).on('keyup paste', Scope.Shadow.synch);
 				$('#sc-find').val(sctxtarea.getSelection().text).focus();
-				Scope.Sequencer('GUI').loaded();
+				sctxtarea.css(commonCSS).after('<div id="sc-shadow"></div>');
+				var commonCSS = {
+					width: '100%', left: 0, top: 0, border: '0 none', display: 'block',
+					outline: 'medium none', margin: 0, padding: 0, resize: 'none'
+				};
+				sctxtarea.css(commonCSS).css({position: 'relative', zIndex: '1',
+					backgroundColor: 'transparent'
+				});
+				sctxtarea.scroll($('#sc-shadow').scrollTop(sctxtarea.scrollTop()));
+				sctxtarea.focus();
+				Scope.Shadow.synch();
 			}				
 		});
-	}());
-
-	/* Layout Shadow's GUI */
-	Scope.Sequencer('GUI').onload(function () {
-		sctxtarea.css(commonCSS).after('<div id="sc-shadow"></div>');
-		var commonCSS = {
-			width: '100%', left: 0, top: 0, border: '0 none', display: 'block',
-			outline: 'medium none', margin: 0, padding: 0, resize: 'none'
-		};
-		sctxtarea.css(commonCSS).css({position: 'relative', zIndex: '1',
-			backgroundColor: 'transparent'
-		});
-		sctxtarea.scroll($('#sc-shadow').scrollTop(sctxtarea.scrollTop()));
-		sctxtarea.focus();
-		Scope.Shadow.synch();
 	}());
