9c66680e116e6f3d3e9eebf2190e0ea3196c51b8
Kangaroopower <racecar360@gmail.com>
2012-08-16 13:32:35 -0700

    tabbing and notes

--- Gui.js
+++ Gui.js
@@ -1,25 +1,22 @@
-/* GUI */
-	/* GUI operations- open/close (Actual dialog is stored elsewhere) */
-	Scope.Sequencer('modules').onload(function () {
-		$('#sc-start').click(function () {
-			if (document.querySelector('#sc-ui')) {
-				$('#sc-shadow, #sc-ui').remove();
-				sctxtarea.removeAttr('style');
-			} else {
-				$('#editform').prepend(Scope.dialog);
-				$('#sc-find, #sc-cs, '+ sctxtarea).on('keyup paste', Scope.Shadow.synch);
-				$('#sc-find').val(sctxtarea.getSelection().text).focus();
-				sctxtarea.css(commonCSS).after('<div id="sc-shadow"></div>');
-				var commonCSS = {
-					width: '100%', left: 0, top: 0, border: '0 none', display: 'block',
-					outline: 'medium none', margin: 0, padding: 0, resize: 'none'
-				};
-				sctxtarea.css(commonCSS).css({position: 'relative', zIndex: '1',
-					backgroundColor: 'transparent'
-				});
-				sctxtarea.scroll($('#sc-shadow').scrollTop(sctxtarea.scrollTop()));
-				sctxtarea.focus();
-				Scope.Shadow.synch();
-			}				
-		});
-	}());
+/* GUI operations- open/close & shadow GUI (Actual dialog is stored elsewhere) */
+Scope.Sequencer('lib').onload(function () {
+	$('#sc-start').click(function () {
+		if (document.querySelector('#sc-ui')) {
+			$('#sc-shadow, #sc-ui').remove();
+			sctxtarea.removeAttr('style');
+		} else {
+			$('#editform').prepend(Scope.dialog);
+			$('#sc-find, #sc-cs, '+ sctxtarea).on('keyup paste', Scope.Shadow.synch);
+			$('#sc-find').val(sctxtarea.getSelection().text).focus();
+			sctxtarea.css(commonCSS).after('<div id="sc-shadow"></div>');
+			var commonCSS = {
+				width: '100%', left: 0, top: 0, border: '0 none', display: 'block',
+				outline: 'medium none', margin: 0, padding: 0, resize: 'none'
+			};
+			sctxtarea.css(commonCSS).css({position: 'relative', zIndex: '1', backgroundColor: 'transparent'});
+			sctxtarea.scroll($('#sc-shadow').scrollTop(sctxtarea.scrollTop()));
+			sctxtarea.focus();
+			Scope.Shadow.synch();
+		}				
+	});
+}());
