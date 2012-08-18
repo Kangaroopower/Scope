bd44f753e345fede9f9056234e296b2812247f5b
Kangaroopower <racecar360@gmail.com>
2012-08-18 14:42:55 -0700

    add undo back in... for now

--- Actions.js
+++ Actions.js
@@ -1,7 +1,6 @@
 /* Does the replace */
 Scope.replace = function (rall) {
-	var replacetxt = $('#sc-replace-text').val(),
-		undotext = sctxtarea.val();
+	var replacetxt = $('#sc-replace-text').val(), undotext = sctxtarea.val();
 	if (rall === true) {
 		var matchIndex = sctxtarea.val().indexOf(scfind), count = 0;
 		while (matchIndex !== -1) {
@@ -14,11 +13,17 @@ Scope.replace = function (rall) {
 		$("#sc-status").html(count + ' replacement(s) made!');
 	} else {
 		if (!$('input#sc-cs').is(':checked')) sctxtarea.val(sctxtarea.val().toLowerCase().replace(scfind, replacetxt));
-		var sel = sctxtarea.getSelection();
 		if (sel.text === "") sctxtarea.val(sctxtarea.val().replace(scfind, replacetxt));
 		else if (sel && scfind.test(sctxtarea.val().substring(sel.start, sel.end))) sctxtarea.val(sctxtarea.val().substring(0, sel.start) + replacetxt + sctxtarea.val().substring(sel.end));
-		Scope.Shadow.highlight(true);
+		Scope.Shadow.dir(true);
 		$("#sc-status").html('One replacement made!');
 	}
+	if (!document.querySelector('#sc-undo')) $('#sc-status').append('<img id="sc-undo"src="https://github.com/Kangaroopower/Scope/raw/master/undo.png" style="vertical-align:middle;"/>');
+	$('#sc-undo').click(function () {
+		sctxtarea.val(undotext);
+		$("#sc-status").html('Undid last replace!');
+		Scope.Shadow.synch();
+		$('#sc-undo').hide();				
+	});
 	Scope.Shadow.synch();
 };
