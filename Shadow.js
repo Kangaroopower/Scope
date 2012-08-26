e5de0b5c0957d44b20088ea35d957f76169f0cb2
Kangaroopower <racecar360@gmail.com>
2012-08-26 16:41:44 -0700

    rewrite; only supports finding next match
    deleted useless code, condensed, doesn't support finding prev match
    anymore

--- Shadow.js
+++ Shadow.js
@@ -1,46 +1,40 @@
-/* Shadow Module */
-(function () {
-	var matches = [], highlighted = -1, nTrav = 0;
-	//Synchs shadow's contents with the contents of the textarea
-	$.sub('synch', function () {
-		console.log('synching');
-		var ref = 0;
-		matches = [];
-		while(sctxtarea.val().indexOf(scfind, ref) !== -1) {
-			if (!$('input#sc-cs').is(':checked')) matches.push(sctxtarea.val().toLowerCase().indexOf(scfind));
-			else matches.push(sctxtarea.val().toLowerCase().indexOf(scfind));
-			ref = sctxtarea.val().indexOf(scfind, ref) + 1;
-		}
-		$('#sc-shadow').html(function () {
-			var r = '', s = sctxtarea.val();
-			for (var i = 0, start = 0; i < matches.length; i++) {
-				r += s.substr(start, matches[i] - start);
-				start = matches[i] + scfind.length;
-				r += '<span id="sc' + i + '" style="background-color:#700066;">' + scfind + '</span>';
-			}
-			if (s.substr(start+1).length > 0) r += s.substr(start+1);
-			return r.length ? r : s;
-		});
-		$('#sc-shadow').css('height', sctxtarea.height()); 
-		$('#sc-shadow').css('width', sctxtarea.width());
-		$('#sc-shadow').scrollTop(sctxtarea.scrollTop());
-	});
-
-	//Does the actual match highlighting
-	function highlight (high) {
-		if (!matches.length) return;
-		sctxtarea.focus();
-		$('#sc' + high).css({backgroundColor:'#0000FF'});
-		highlighted = high;
-		sctxtarea.setSelection(matches[high], matches[high] + scfind.length);
-		if (nTrav === matches.length) nTrav = 0;
-		nTrav++;
-		$('#sc-count').html(nTrav + ' of ' + matches.length);
+//Synchs shadow with the textarea
+$.sub('synch', function () {
+	console.log('synching');
+	var ref = 0, s = sctxt.val();
+	matches = [];
+	while(s.indexOf(scfind, ref) !== -1) {
+		if (!$('#sc-cs').is(':checked')) matches.push(s.toLowerCase().indexOf(scfind));
+		else matches.push(s.toLowerCase().indexOf(scfind));
+		ref = s.indexOf(scfind, ref) + 1;
 	}
+	$('#sc-shadow').html(function () {
+		var r = '';
+		for (var i = 0, start = 0; i < matches.length; i++) {
+			r += s.substr(start, matches[i] - start);
+			start = matches[i] + scfind.length;
+			r += '<span id="sc' + i + '"class="sc-match">' + scfind + '</span>';
+		}
+		if (s.substr(start+1).length > 0) r += s.substr(start+1);
+		return r.length ? r : s;
+	});
+	$('#sc-shadow').css('height', sctxt.height()); 
+	$('#sc-shadow').css('width', sctxt.width());
+	$('#sc-shadow').scrollTop(sctxt.scrollTop());
+});
 
-	//This exists because I am lazy... integrate into highlight
-	Scope.dir = function  (next) {
-		if (next === true) for (var n = 0; n < matches.length; n++) if (sel.end < matches[n] + matches[n].length) highlight(n);
-		else for (var p = matches.length-1; p >= 0; p--) if (sel.start > matches[p]) highlight(p);
-	};
-})();
+//Highlights next match
+$.sub('next', function  () {
+	var nTrav = 0;
+	if (!matches.length) return;
+	sctxt.focus();
+	for (var n = 0; n < matches.length; n++) {
+		if (sel.end < matches[n] + matches[n]l.ength) {
+			$('#sc' + n).css({backgroundColor:'#0000FF'});
+			sctxt.setSelection(matches[n], matches[n] + scfind.length);
+		}
+	}
+	if (nTrav === matches.length) nTrav = 0;
+	nTrav++;
+	$('#sc-count').html(nTrav + ' of ' + matches.length).attr('title', '');
+});
