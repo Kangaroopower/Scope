36888b761570b4006d2683d742cab469bdd2dc23
Kangaroopower <racecar360@gmail.com>
2012-08-26 16:42:37 -0700

    shorten, add close operation

--- Gui.js
+++ Gui.js
@@ -1,20 +1,22 @@
 /* GUI operations- open/close (Actual dialog is stored at Mediawiki:Scope/gui.js) */
 $.sub('open', function () {
-	if (document.querySelector('#sc-ui')) {
-		$('#sc-shadow, #sc-ui').remove();
-		sctxtarea.removeAttr('style');
-	} else {
-		$('#editform').prepend(Scope.dialog);
-		$('#sc-find, #sc-cs, '+ sctxtarea).on('keyup paste', $.pub('synch'));
+	if (document.querySelector('#sc-ui')) $.pub('close');
+	else {
+		$('.cke_toolbar_expand').after(Scope.dialog);
+		if (matches.length) $('#sc-down').css({cursor: 'pointer'});
+		$('#sc-find, #sc-cs, '+ sctxt).on('keyup paste', $.pub('synch'));
 		$('#sc-find').val(sel.text).focus();
 		var commonCSS = {
 			width: '100%', left: 0, top: 0, border: '0 none', display: 'block',
 			outline: 'medium none', margin: 0, padding: 0, resize: 'none'
 		};
-		sctxtarea.css(commonCSS).css({position: 'relative', zIndex: '1'}).after('<div id="sc-shadow"></div>');
-		$('#sc-shadow').css(commonCSS)
-		sctxtarea.scroll($('#sc-shadow').scrollTop(sctxtarea.scrollTop()));
-		sctxtarea.focus();
+		sctxt.css(commonCSS).after('<div id="sc-shadow"></div>').focus();
+		$('#sc-shadow').css(commonCSS);
 		$.pub('synch');
 	}
+});
+
+$.sub('close', function () {
+	$('#sc-shadow, #sc-ui, #sc-style').remove();
+	sctxt.removeAttr('style');
 });
