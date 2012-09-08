f1384574c5833410946f7c9a314a021c5888ece2
Kangaroopower <racecar360@gmail.com>
2012-09-07 19:56:19 -0700

    bugfixes

--- Scope.js
+++ Scope.js
@@ -1,32 +1,48 @@
+/**
+ * Find and Replace 3.0 Alpine
+ * 
+ * Creates a Find and Replace Module
+ *
+ * @author Kangaroopower
+ * @Shadow author Pecoes
+ *
+ */
 $(function () {
 	//Meta Vars
-	var sctxt, sel, scfind = $('#sc-find').val(), matches = [], root = '//raw.github.com/Kangaroopower/Scope/master';
+	var sctxt, matches = [], root = '//raw.github.com/Kangaroopower/Scope/master';
 
 	//Base for functions
 	window.Scope = {
 		version: "3.0 Alpine",
 		lib: {
-			'TextInputs': root+'/lib/Rangy.js',
 			'Dialog': root+'/lib/Dialog.js',
+			'Rangy': root+'/lib/Rangy.js',
 			'jQueryUI': 'http://code.jquery.com/ui/1.9.0-rc.1/jquery-ui.min.js'
-		},
-		subs: {}
+		}
 	};
 
-	var Scope = window.Scope;
+	var ns = window.Scope;
+
+	/* Logs stuff */
+	ns.log = (window.console && function () {
+		var args = Array.prototype.slice.call(arguments);
+		args.unshift('Scope: ');
+		return window.console.log.apply(window.console, args);
+	}) || $.noop;
 
 	/* Controls loading process */
 	(function () {
+		var subs = {};
 		$.extend({
 			scribe: function (topic, callback) {
-			if (!Scope.subs[topic]) Scope.subs[topic] = $.Callbacks();
-				Scope.subs[topic].add(callback);
+			if (!subs[topic]) subs[topic] = $.Callbacks();
+				subs[topic].add(callback);
 			},
 			pub: function (topic) {
-				if (!Scope.subs[topic]) console.log('unknown topic');
+				if (!subs[topic]) ns.log('unknown topic');
 				else {
 					var parameters = Array.prototype.splice.call(arguments, 1);
-					Scope.subs[topic].fire.apply(null, parameters);
+					subs[topic].fire.apply(null, parameters);
 				}
 			}
 		});
@@ -37,37 +53,44 @@ $(function () {
 		if (window.RTE && RTE.getInstance && RTE.getInstance()) {
 			if (RTE.getInstance().mode === 'source') $.pub('editor');
 			else if(RTE.getInstance().mode === 'wysiwyg') $.pub('close');
-			else console.log('Cannot detect editor');
+			else ns.log('Cannot detect editor');
 		} else if (window.CKEDITOR) {
 			CKEDITOR.on('instanceReady', function () {
-				RTE.getInstance().on('wysiwygModeReady', $.pub('editor'));
-				RTE.getInstance().on('sourceModeReady', $.pub('editor'));
+				RTE.getInstance().on('wysiwygModeReady', function () {
+					$.pub('editor');
+				});
+				RTE.getInstance().on('sourceModeReady', function () {
+					$.pub('editor');
+				});
 			});
 		} else if (window.WikiaEditor) {
 			if (WikiaEditor.getInstance && WikiaEditor.getInstance()) {
 				if (WikiaEditor.getInstance().mode === 'source') $.pub('editor');
 				else $.pub('close');
-			} else if (GlobalTriggers) GlobalTriggers.on('WikiaEditorReady', $.pub('editor'));
-			else console.log('Cannot detect editor');
-		} else console.log('Cannot detect editor');
+			} else if (GlobalTriggers) GlobalTriggers.on('WikiaEditorReady', function () {
+				$.pub('editor');
+			});
+			else ns.log('Cannot detect editor');
+		} else ns.log('Cannot detect editor');
 	});
 
 	/* Load libraries before actual script */
 	$.scribe('editor', function () {
-		console.log('Scope: Editor Loaded');
-		for (var i in Scope.lib) $.getScript(Scope.lib[i], console.log('Scope: '+i+' loaded'));
+		ns.log('Scope: Editor Loaded');
+		for (var i in ns.lib) $.getScript(ns.lib[i], ns.log('Scope: '+i+' loaded'));
 		sctxt = WikiaEditor.getInstance().getEditbox();
-		sel = sctxt.getSelection();
 		$('span.cke_toolbar_expand').before('<img id="sc-start" src="'+root+'/util/Replace.png"/>');
-		$('#sc-start').click($.pub('open'));
-		console.log('Loaded: Scope', Scope.version);
+		$('#sc-start').click(function () {
+			$.pub('open');
+		});
+		ns.log('Loaded: Scope', ns.version);
 	});
 
 	/* GUI operations- open/close (Actual dialog is stored at Mediawiki:Sc/gui.js) */
 	$.scribe('open', function () {
 		if (document.querySelector('#sc-ui')) $.pub('return');
 		else {
-			$('.cke_toolbar_expand').after(Scope.dialog);
+			$('.cke_toolbar_expand').after(ns.dialog);
 			$('#sc-cog').click(function (e) {
 				e.preventDefault();
 				if ($('#sc-drop').css('display') === 'none') $('#sc-drop').show();
@@ -75,8 +98,10 @@ $(function () {
 			});
 			$('#sc-count').tooltip();
 			if (matches.length) $('#sc-down').css({cursor: 'pointer'});
-			$('#sc-find, #sc-cs, '+ sctxt).on('keyup paste', $.pub('synch'));
-			$('#sc-find').val(sel.text).focus();
+			$('#sc-find, #sc-cs, '+ sctxt).on('keyup paste', function () {
+				$.pub('synch');
+			});
+			$('#sc-find').val(sctxt.getSelection().text).focus();
 			var commonCSS = {
 				width: '100%', left: 0, top: 0, border: '0 none', display: 'block',
 				outline: 'medium none', margin: 0, padding: 0, resize: 'none'
@@ -96,19 +121,19 @@ $(function () {
 	$.scribe('replace', function () {
 		var rtxt = $('#sc-replace-text').val(), s = sctxt.val(), undotext = s;
 		if ($('#sc-rall').is(':checked')) {
-			var matchIndex = s.indexOf(scfind), count = 0;
+			var matchIndex = s.indexOf($('#sc-find').val()), count = 0;
 			while (matchIndex !== -1) {
-				if (!$('#sc-cs').is(':checked')) sctxt.val(s.toLowerCase().replace(scfind, rtxt));
-				else sctxt.val(s.replace(scfind, rtxt));
-				matchIndex = s.indexOf(scfind);
+				if (!$('#sc-cs').is(':checked')) sctxt.val(s.toLowerCase().replace($('#sc-find').val(), rtxt));
+				else sctxt.val(s.replace($('#sc-find').val(), rtxt));
+				matchIndex = s.indexOf($('#sc-find').val());
 				count++;
 			}
 			if (count === 1) count = "One";
 			$("#sc-count").html('Done!').attr('title', count + ' replacement(s) made!');
 		} else {
-			if (!$('#sc-cs').is(':checked')) sctxt.val(s.toLowerCase().replace(scfind, rtxt));
-			if (sel.text === "") sctxt.val(s.replace(scfind, rtxt));
-			else if (scfind.test(s.substring(sel.start, sel.end))) sctxt.val(s.substring(0, sel.start) + rtxt + s.substring(sel.end));
+			if (!$('#sc-cs').is(':checked')) sctxt.val(s.toLowerCase().replace($('#sc-find').val(), rtxt));
+			if (sctxt.getSelection().text === "") sctxt.val(s.replace($('#sc-find').val(), rtxt));
+			else if ($('#sc-find').val().test(s.substring(sctxt.getSelection().start, sctxt.getSelection().end))) sctxt.val(s.substring(0, sctxt.getSelection().start) + rtxt + s.substring(sctxt.getSelection().end));
 			$.pub('next');
 			$("#sc-count").html('Done!').attr('title', 'One replacement made!');
 		}
@@ -124,20 +149,20 @@ $(function () {
 	
 	//Synchs shadow with the textarea
 	$.scribe('synch', function () {
-		console.log('synching');
+		ns.log('synching');
 		var ref = 0, s = sctxt.val();
 		matches = [];
-		while(s.indexOf(scfind, ref) !== -1) {
-			if (!$('#sc-cs').is(':checked')) matches.push(s.toLowerCase().indexOf(scfind));
-			else matches.push(s.toLowerCase().indexOf(scfind));
-			ref = s.indexOf(scfind, ref) + 1;
+		while(s.indexOf($('#sc-find').val(), ref) !== -1) {
+			if (!$('#sc-cs').is(':checked')) matches.push(s.toLowerCase().indexOf($('#sc-find').val()));
+			else matches.push(s.toLowerCase().indexOf($('#sc-find').val()));
+			ref = s.indexOf($('#sc-find').val(), ref) + 1;
 		}
 		$('#sc-shadow').html(function () {
 			var r = '';
 			for (var i = 0, start = 0; i < matches.length; i++) {
 				r += s.substr(start, matches[i] - start);
-				start = matches[i] + scfind.length;
-				r += '<span id="sc' + i + '"class="sc-match">' + scfind + '</span>';
+				start = matches[i] + $('#sc-find').val().length;
+				r += '<span id="sc' + i + '"class="sc-match">' + $('#sc-find').val() + '</span>';
 			}
 			if (s.substr(start+1).length > 0) r += s.substr(start+1);
 			return r.length ? r : s;
@@ -153,9 +178,9 @@ $(function () {
 		if (!matches.length) return;
 		sctxt.focus();
 		for (var n = 0; n < matches.length; n++) {
-			if (sel.end < matches[n] + matches[n].length) {
+			if (sctxt.getSelection().end < matches[n] + matches[n].length) {
 				$('#sc' + n).css({backgroundColor:'#0000FF'});
-				sctxt.setSelection(matches[n], matches[n] + scfind.length);
+				sctxt.setSelection(matches[n], matches[n] + $('#sc-find').val().length);
 			}
 		}
 		if (nTrav === matches.length) nTrav = 0;
