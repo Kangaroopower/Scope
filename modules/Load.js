7a13a48a0e61d80f298224f37757c79003656fca
Kangaroopower <racecar360@gmail.com>
2012-09-23 21:45:21 -0700

    much much much better load pattern, no pub/sub

--- modules/Load.js
+++ modules/Load.js
@@ -7,82 +7,92 @@
  * @Shadow author Pecoes
  *
  */
-$(function( ) {
+(function() {
 	//Meta Vars
-	var sctxt, sel, scfind = $('#sc-find').val(), matches = [], root ='https://raw.github.com/Kangaroopower/Scope/master';
+	var scfind, sctxt, root ='https://raw.github.com/Kangaroopower/Scope/master';
 
-	//Base for functions
+	//Scope Class
 	window.Scope = {
 		version: "3.0 Alpine",
-		libraries: {
-			'TextInputs': root+'/lib/Rangy.js',
-			'Dialog': root+'/lib/Dialog.js',
-			'jQueryUI': 'http://code.jquery.com/ui/1.9.0-rc.1/jquery-ui.min.js'
-		},
-		modules: {
-			'Actions': root+'/modules/Actions.js',
-			'GUI': root+'/modules/Gui.js',
-			'Shadow': root+'/modules/Shadow.js'
-		}
+		lib: [
+				{ name: 'Dialog', url: root+'/Dialog.js' },
+				{ name: 'Rangy', url: 'http://dev.wikia.com/wiki/Textinputs_jquery.js?action=raw&ctype=text/javascript' }
+			],
+		modules: [
+				{ name: 'Actions', url: 'Actions.js' },
+				{ name: 'GUI', url: 'Gui.js' },
+				{ name: 'GUI', url: 'Shadow.js' }
+			]
 	};
 
-	var Sc = window.Scope;
+	var Scope = window.Scope;
 
-	/* Controls loading process */
-	(function () {
-		var subs = {};
-		//Adds an event listener
-		jQuery.csub = function (type, func) {
-			if(!subs[type]) subs[type] = [];
-			subs[type].push(func);
-			return true;
-		};
 
-		//Calls an event listener
-		jQuery.psub = function (type) {
-			if(!subs[type]) return false;
-			if(subs[type].length === 0) return true;
-			var args = [];
-			for(var i = 1; i < arguments.length; i++) args.push(arguments[i]);
-			for(var j = 0; j < subs[type].length; j++) subs[type][j].apply(this, args);
-			return true;
-		};
-	}());
-
-	/* Load editor before everything else (and make sure it's in source mode!)*/
-	$.csub('doc', function () {
+	/* Logs stuff */
+	var log = (window.console && function () {
+		var args = Array.prototype.slice.call(arguments);
+		args.unshift('Scope:');
+		return window.console.log.apply(window.console, args);
+	}) || $.noop;
+ 
+	/* Load libraries first */
+	function load () {
+		if (mw.config.get('wgAction') === 'edit'  && [1200,1201].indexOf(mw.config.get('wgNamespaceNumber')) !== -1 ) return;
+		var loaded = 0,
+			onload = function (name) {
+				return function () {
+					log(name + ' loaded');
+					if (++loaded === Scope.lib.length) $(editor);
+				};
+			};
+		for (var i = 0; i < Scope.lib.length; i++) {
+			console.log('loading ', Scope.lib[i].name, '...');
+			$.getScript(Scope.lib[i].url, onload(Scope.lib[i].name));
+		}
+	}
+ 
+	/* Check if editor has loaded after libraries have */
+	function editor () {
+		log('doc');
 		if (window.RTE && RTE.getInstance && RTE.getInstance()) {
-			if ('source' === RTE.getInstance().mode) $.psub('editor');
-			else $.psub('close');
+			if (RTE.getInstance().mode === 'source') setup();
+			else if(RTE.getInstance().mode === 'wysiwyg') hide();
+			else log('Cannot detect editor');
 		} else if (window.CKEDITOR) {
 			CKEDITOR.on('instanceReady', function () {
-				RTE.getInstance().on('wysiwygModeReady', $.psub('close'));
-				RTE.getInstance().on('sourceModeReady', $.psub('editor'));
+				RTE.getInstance().on('wysiwygModeReady', hide);
+				RTE.getInstance().on('sourceModeReady', setup);
 			});
 		} else if (window.WikiaEditor) {
 			if (WikiaEditor.getInstance && WikiaEditor.getInstance()) {
-				if ('source' === WikiaEditor.getInstance().mode) $.psub('editor');
-				else $.psub('close');
-			} else if (GlobalTriggers) GlobalTriggers.on('WikiaEditorReady', $.psub('editor'));
-		}
-	});
-	/* Load libraries before actual script */
-	$.csub('editor', function () {
-		for (var i in Scope.libraries) $.getScript(Scope.libraries[i], console.log('Scope: '+i+' loaded'));
+				if (WikiaEditor.getInstance().mode === 'source') setup();
+				else hide();
+			} else if (GlobalTriggers) GlobalTriggers.on('WikiaEditorReady', setup);
+			else log('Cannot detect editor');
+		} else log('Cannot detect editor');
+	}
+ 
+	/* Load modules */
+	function setup () {
+		log('Editor Loaded');
 		sctxt = WikiaEditor.getInstance().getEditbox();
-		sel = sctxt.getSelection();
-		console.log('Scope: Editor Loaded');
-		$.psub('modules');
-	});
-
-	/* Load modules before script */
-	$.csub('modules', function () {
-		for (var i in Scope.modules) $.getScript(Scope.modules[i], console.log('Scope: '+i+' loaded'));
-		$('span.cke_toolbar_expand').before('<img id="sc-start" src="'+root+'/util/Replace.png"/>');
-		$('#sc-start').click($.psub('open'));
-		console.log('Loaded: Scope', Scope.version);
-	});
+		var loaded = 0,
+			onload = function (name) {
+				return function () {
+					log(name + ' loaded');
+					if (++loaded === Scope.modules.length) {
+						if (!$('#sc-start').length) $('span.cke_toolbar_expand').before('<img id="sc-start" src="//raw.github.com/Kangaroopower/Scope/master/util/Replace.png"/>');
+						$('#sc-start').click(show);
+						log('Loaded: Scope', Scope.version);
+					}
+				};
+			};
+		for (var i = 0; i < Scope.modules.length; i++) {
+			console.log('loading ', Scope.modules[i].name, '...');
+			$.getScript(root+'/modules/'+Scope.modules[i].url, onload(Scope.modules[i].name));
+		}
+	}
 
 	//Load on edit
-	if (wgAction === 'edit' && wgNamespaceNumber !== (1200||1201)) $.psub('doc');
+	$(load);
 });
