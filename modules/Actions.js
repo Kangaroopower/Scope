1940d15bbe6d4a612574b15dff31297fbab864d2
Kangaroopower <racecar360@gmail.com>
2012-09-23 21:44:32 -0700

    bring in regex, no more pub/sub

--- modules/Actions.js
+++ modules/Actions.js
@@ -1,29 +1,32 @@
-/* Does the replace */
-$.csub('replace', function () {
-	var replacetxt = $('#sc-replace-text').val(), s = sctxt.val(), undotext = s;
-	if ($('#sc-rall').is(':checked')) {
-		var matchIndex = s.indexOf(scfind), count = 0;
-		while (matchIndex !== -1) {
-			if (!$('#sc-cs').is(':checked')) sctxt.val(s.toLowerCase().replace(scfind, replacetxt));
-			else sctxt.val(s.replace(scfind, replacetxt));
-			matchIndex = s.indexOf(scfind);
-			count++;
-		}
-		if (count === 1) count = "One";
-		$("#sc-count").html('Done!').attr('title', count + ' replacement(s) made!');
-	} else {
-		if (!$('#sc-cs').is(':checked')) sctxt.val(s.toLowerCase().replace(scfind, replacetxt));
-		if (sel.text === "") sctxt.val(s.replace(scfind, replacetxt));
-		else if (scfind.test(s.substring(sel.start, sel.end))) sctxt.val(s.substring(0, sel.start) + replacetxt + s.substring(sel.end));
-		$.psub('next');
-		$("#sc-count").html('Done!').attr('title', 'One replacement made!');
+	/* Evaluates the regex to be used */
+	function evaluate (rall) {
+		var mod = rall ? 'g' : '';
+		if (!$('#sc-cs').hasClass('scactive')) mod += 'i';
+		if ($('#sc-reg').hasClass('scactive')) return new RegExp(scfind.val(), mod);
+		else return new RegExp(scfind.val().replace(/\[\-[\]{}()*+?.,\\\^$|#\s]/g, "\\$&"), mod);
 	}
-	if (!$('#sc-undo').length) $('#sc-replace-text').append('<img id="sc-undo"src="'+root+'/util/undo.png"/>');
-	$('#sc-undo').click(function () {
-		sctxt.val(undotext);
-		$("#sc-count").html('Undone!').attr('title', '');
-		$.psub('synch');
-		$('#sc-undo').hide();
-	});
-	$.psub('synch');
-});
+ 
+	/* Does the replace */
+	function replace (rall) {
+		var rtxt = $('#sc-replace-text').val(), s = sctxt.val(), undotext = sctxt.val();
+		if (rall === true) {
+			var count;
+			if (evaluate(true).length === 1) count = "One";
+			else count = s.match(evaluate(true)).length;
+			sctxt.val(s.replace(evaluate(true), rtxt));
+			$("#sc-count").html('Done!').attr('title', count + ' replacement(s) made!');
+		} else {
+			if (sctxt.getSelection().text === "") sctxt.val(s.replace(evaluate(), rtxt));
+			else if (scfind.val().test(s.substring(sctxt.getSelection().start, sctxt.getSelection().end)))sctxt.val(s.substring(0, sctxt.getSelection().start) + rtxt + s.substring(sctxt.getSelection().end));
+			next();
+			$("#sc-count").html('Done!').attr('title', 'One replacement made!');
+		}
+		if (!$('#sc-undo').length) $('#sc-replace-text').after('<img id="sc-undo"src="//raw.github.com/Kangaroopower/Scope/master/util/undo.png"/>');
+		$('#sc-undo').click(function () {
+			sctxt.val(undotext);
+			$("#sc-count").html('Undone!').attr('title', '');
+			synch();
+			$('#sc-undo').hide();
+		});
+		synch();
+	}
