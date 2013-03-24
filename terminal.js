be9399729a6b8e2e2ebaf7fd4de136acaadbb176
Kangaroopower <racecar360@gmail.com>
2013-03-23 20:10:14 -0700

    move terminal out of alpha, make it work

--- terminal.js
+++ terminal.js
@@ -4,17 +4,13 @@
  * 
  * Creates a Find and Replace Terminal
  *
- * Notes:
- *		- Just an experriment.
- * 		- In Alpha
- *
  * @author Kangaroopower
  *
  */
 (function () {
 
 	//Meta Vars
-	var sctxt, undotext = null, version = "3.0 Terminal";
+	var sctxt, undotext = '', version = "3.1 Terminal";
 
 	/* Logs stuff */
 	var log = (window.console && function () {
@@ -49,17 +45,16 @@
 	function setup () {
 		log('Editor Loaded');
 		sctxt = WikiaEditor.getInstance().getEditbox();
-		if (!$('#sc-terminal-start').length) $('span.cke_toolbar_expand').before('<img id="sc-terminal-start" src="//raw.github.com/Kangaroopower/Scope/master/util/Replace.png"/>');
+		if (!$('#sc-terminal-start').length) $('span.cke_toolbar_expand').before('<img style="cursor:pointer;" id="sc-terminal-start" src="//raw.github.com/Kangaroopower/Scope/master/pics/Replace.png"/>');
 		$('#sc-terminal-start').click(show);
 		log('Loaded version:', version);
 	}
  
 	/* Opens and sets up gui */
 	function show () {
-		if ('')
 		log('opening dialog');
 		if ($('#sc-terminal').length) return $('#sc-terminal').remove();
-		$('.cke_toolbar_expand').after('<div id="sc-terminal" style="z-index: 1001;"><table cellspacing="0"><tbody><tr><td><table cellspacing="0"><tbody><tr><td style="padding-bottom: 5px;"><table cellpadding="0" cellspacing="0" style="border-radius: 4px;background: white;border: 1px solid #D9D9D9;border-top: 1px solid silver;padding: 0 8px;width: 598px;"><tbody><tr><td><input type="text" id="sc-text" placeholder="Find and Replace..." style="width: 390px;border: none!important;height: 20px;outline: none!important;border-radius: 4px;"></td><td id="sc-tcount" style="max-width: 300px;overflow: hidden;padding-right: 2px;text-align: right;"></td></tr></tbody></table></td></tr></tbody></table></td></tr></tbody></table></div>');
+		$('span.cke_toolbar_expand').after('<div id="sc-terminal" style="z-index: 1001;padding-bottom: 5px;padding-top: 5px;"><input type="text" id="sc-text" placeholder="Find and Replace..." style="border-radius: 4px;background: white;border: 1px solid #D9D9D9;border-top: 1px solid silver;padding: 0 8px;width: 390px;height: 20px;outline: none!important;"><span id="sc-tcount" style="padding-left: 10px;"></span></div>');
 		$('#sc-text').keydown(function (e) {
 			if(e.which === 13) {
 				e.preventDefault();
@@ -67,27 +62,20 @@
 			}
 		});
 	}
- 
 
 	/* Parses input from "terminal" */
 	function evaluate () {
-		if ($('#sc-text').val() === '#undo') {
-			if (undotext === null) {
-				$('#sc-tcount').html('Could not perform undo');
-			} else {
-				sctxt.val(undotext);
-				undotext = null;
-			}
+		var tokenized = $('#sc-text').val().split('/');
+		if (tokenized[0] === "#SANITIZE") {
+			//escaping: courtesy of http://stackoverflow.com/questions/3446170/
+			var escaped = tokenized[1].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"),
+				secondsplit = tokenized[3].split(' ');
+			//you skip an array piece for the replace because you want a "with" to be in between
+			//eg: /a/ig with b
+			replace(new RegExp(escaped, $.trim(secondsplit[0])), secondsplit[2]); 
 		} else {
-			var tokenized = $('#sc-text').val().split('  '); //Temporary ugly solution
-			for (var i = 0; i < tokenized.length; i++) {
-				tokenized = tokenized[i].replace(/__/ig, '  ');
-			}
-			if (tokenized[0] === "#SANITIZE") {
-				replace(new RegExp(tokenized[1].replace(/\[\-[\]{}()*+?.,\\\^$|#\s]/g, "\\$&"), (tokenized[2] || 'g')), tokenized[4]);
-			} else {
-				replace(new RegExp(tokenized[0], (tokenized[1] || 'g')), tokenized[3]);
-			}
+			var secondsplit = tokenized[2].split(' ');
+			replace(new RegExp(tokenized[1], $.trim(secondsplit[0])), secondsplit[2]);
 		}	
 	}
  
@@ -95,7 +83,13 @@
 	function replace (regex, rtxt) {
 		undotext = sctxt.val();
 		sctxt.val(sctxt.val().replace(regex, rtxt));
-		$("#sc-tcount").html('Done!').attr('title', sctxt.val().match(evaluate()).length + ' replacement(s) made!');
+		$("#sc-tcount").html('Done! '+ sctxt.val().match(evaluate()).length + ' replacement(s) made!');
+		if (!$('#sc-undo').length) $('#sc-replace-text').after('<img id="sc-undo" style="height:20px;cursor:pointer;vertical-align:middle" src="//raw.github.com/Kangaroopower/Scope/master/pics/undo.png">');
+		$('#sc-undo').click(function () {
+			sctxt.val(undotext);
+			$("#sc-tcount").html('Undone!');
+			$('#sc-undo').hide();
+		});
 	}
 
 	//Load on edit
