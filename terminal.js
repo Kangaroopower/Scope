daf90ac324130a1619ccbf6962581ca2146758db
Kangaroopower <racecar360@gmail.com>
2013-04-16 20:16:09 -0700

    bfs for not letting it close, input errors

--- terminal.js
+++ terminal.js
@@ -58,7 +58,10 @@
     /* Opens and sets up gui */
     function show () {
         log('opening dialog');
-        $('#sc-terminal').remove();
+         if ($('#sc-terminal').length) {
+            $('#sc-terminal').remove();
+            return;
+         }
         $('span.cke_toolbar_expand').after('<div id="sc-terminal" style="z-index: 1001;padding-bottom: 5px;padding-top: 5px;"><input type="text" id="sc-text" placeholder="Find and Replace..." style="border-radius: 4px;background: white;border: 1px solid #D9D9D9;border-top: 1px solid silver;padding: 0 8px;width: 390px;height: 20px;outline: none!important;"><span id="sc-tcount" style="padding-left: 10px;"></span></div>');
         $('#sc-text').keydown(function (e) {
             if(e.which === 13) {
@@ -73,28 +76,33 @@
         $("#sc-text").css({"background-color": "white"});
         $("#sc-tcount").html("");
 
-        var secondsplit, t = $('#sc-text').val().split('/');
-        secondsplit = (t[0].toLowerCase() === '#sanitize') ? t[3].split(' ') : t[2].split(' ');
+        var ts, tr = $('#sc-text').val().split('/'), v = $('#sc-text').val();
+        ts = (tr[0].toLowerCase() === '#sanitize') ? tr[3].split(' ') : tr[2].split(' ');
         
-        if (!/^\/.+\/(i|g|m)*$/.test("/" + t[1] + "/" + $.trim(secondsplit[0]))) {
+        if (!/^\/.+\/(i|g|m)*$/.test("/" + tr[1] + "/" + $.trim(ts[0])) || !/with/ig.test(v)) {
             $("#sc-text").css({"background-color": "#f2dede"});
             $("#sc-tcount").html("Input Error");
             return false;
         }
 
+        if (!((tr[1].match(/with/).length + 1) === v.match(/with/).length)) {
+            $("#sc-text").css({"background-color": "#f2dede"});
+            $("#sc-tcount").html("Input Error");
+            return false;
+        }
         if ($("#sc-text").val().toLowerCase() === "#undo") {
             undo();
             undotext = sctxt.val();
         }
 
-        if (t[0].toLowerCase() === '#sanitize') {
+        if (tr[0].toLowerCase() === '#sanitize') {
             //escaping: courtesy of http://stackoverflow.com/questions/3446170/
-            var escaped = t[1].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+            var escaped = tr[1].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
             //you skip an array piece for the replace because you want a 'with' to be in between
             //eg: /a/ig with b
-            replace(new RegExp(escaped, $.trim(secondsplit[0])), secondsplit[2]); 
+            replace(new RegExp(escaped, $.trim(ts[0])), ts[2]); 
         } else {
-            replace(new RegExp(t[1], $.trim(secondsplit[0])), secondsplit[2]);
+            replace(new RegExp(tr[1], $.trim(ts[0])), ts[2]);
         }    
     }
 
